33af016831e659c6a729cd380ea8df76637dcabc
havenec <havenec@wwu.edu>
2023-11-07 19:29:22 -0800

    adding a1_d back

--- oop345/a1_D/Program.java
+++ oop345/a1_D/Program.java
@@ -51,7 +51,7 @@ public class Program
             backpack.removeItemFromLeftPocket(itemName);
 
             itemName = "Fork";
-            backpack.removeItemFromLeftPocket(itemName);
+            //backpack.removeItemFromLeftPocket(itemName);
             
             double backPackTotalWeight = backpack.getTotalWeight();
             System.out.println("Backpack total weight: " + backPackTotalWeight);
